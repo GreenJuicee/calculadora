7a8e7f9f171285336d6fcc7a5f486e5f62439b48
GreenJuicee <milenarita18d@gmail.com>
2024-03-25 20:42:21 -0300

    adicionamdo arquivos

--- calculadora.js
+++ calculadora.js
@@ -6,7 +6,6 @@ function voltar(){
     var resultado = document.getElementById('resultado').innerHTML
     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1)
 }
-
 function inserir(botao){
     var numero = document.getElementById('resultado').innerHTML
     document.getElementById('resultado').innerHTML = numero + botao
